3cb25ba2370d79fefcac5572ae57cc96779701ea
efraimt <efraimt@gmail.com>
2022-03-01 21:25:49 +0200

    Add project files.

---
+++ HackerUs/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Wdgt;l87541"); // W
+Console.WriteLine(87 - (7 * 2)); // 73
+Console.WriteLine(28.700000000000);// 28.7
+Console.WriteLine("28.7000000000");
+Console.WriteLine("87"); //87
+
+
+string studentName = "Moseh";
+int studentAge = 29;
+double studentGrade = 99.2;
+
+
+Console.WriteLine(studentName); // Moshe
+Console.WriteLine(studentAge); // 29
