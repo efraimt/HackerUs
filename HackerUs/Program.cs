858f372f476b2a8388337efa61fe899c1ad7480a
efraimt <efraimt@gmail.com>
2022-03-06 21:36:04 +0200

    Lesson 3

--- HackerUs/Program.cs
+++ HackerUs/Program.cs
@@ -1,14 +1,98 @@
-﻿Console.WriteLine("Wdgt;l87541"); // W
-Console.WriteLine(87 - (7 * 2)); // 73
-Console.WriteLine(28.700000000000);// 28.7
-Console.WriteLine("28.7000000000");
-Console.WriteLine("87"); //87
+﻿//Console.WriteLine("Wdgt;l87541"); // W
+Console.WriteLine(87 - 7 * 2); // 73
+//Console.WriteLine(28.700000000000);// 28.7
+//Console.WriteLine("28.7000000000");
+//Console.WriteLine("87"); //87
 
 
-string studentName = "Moseh";
-int studentAge = 29;
-double studentGrade = 99.2;
+//string studentName = "Moseh";
+//int studentAge = 29;
+//double studentGrade = 99.2;
 
 
-Console.WriteLine(studentName); // Moshe
-Console.WriteLine(studentAge); // 29
+//Console.WriteLine(studentName); // Moshe
+//Console.WriteLine(studentAge); // 29
+
+//the next line prints the size of char
+Console.WriteLine(sizeof(char));
+
+Console.WriteLine("Sapir"); //Sapir
+string s = "Sapir";
+Console.WriteLine(s);
+
+int x = 6;
+Console.WriteLine(x); // 6
+x = -9875;
+Console.WriteLine(x); // -9875
+
+string s1 = "Welcome";
+int x1 = 54;
+
+
+double d1 = 3.254;
+
+int x2;
+x2 = 0;
+Console.WriteLine(x2);
+
+x1 = 3;
+x2 = -5 + x1 + 6 / 2 - 11;
+Console.WriteLine(-5 + x1 + 6 / 2 - 11);
+Console.WriteLine(d1 + 3 + x2);
+
+
+
+
+
+int y4 = 10 / 10;// 1
+y4 = 10 % 10; // 0
+
+//int y4 = 10 / 10;// 1
+//y4 = 10 % 10; // 0
+
+
+
+
+
+
+
+
+
+x2 = 6 % 4; // 2
+x2 = 13 % 10; // 3
+
+x2 = 20 / 2; // 10
+x2 = 23 % 4;
+Console.WriteLine(x2);
+
+
+int number = 2198;
+int yechidot = number % 10; // 8
+int asarot = number % 100 / 10; //9
+int meot = number % 1000 / 100;
+
+//Arithmetic Operators
+// +, -, * , /, ^, %
+
+// Asignment Operators
+// = , 
+
+x2 = x1;
+
+string s3 = "Hello " + "25"; // "Hello 25"
+string firstName = "Sapir";
+string lastName = "Cohen";
+int age = 19;
+
+int v1, v2 = 0, v3 = -54;
+Console.WriteLine(v3+12.5);
+
+Console.WriteLine("Welcome " + firstName + " " + lastName + " you are " + age + " years old!");
+firstName = "Lior";
+
+firstName = Console.ReadLine();
+//v1 = Console.ReadLine();
+Console.ReadLine();
+
+
+
---
+++ IO/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, Welcome to HackerUs");
+Console.WriteLine("Please enter first name");
+string firstName = Console.ReadLine();
+Console.WriteLine("Please enter last name");
+string lastName = Console.ReadLine();
+Console.WriteLine("Please enter date of birth");
+int yearOfBirth = int.Parse(Console.ReadLine()); 
+
+
+Console.Clear();
+Console.Write("Welcome " + firstName + " " + lastName);
+Console.WriteLine(" You are " + (2022 - yearOfBirth) +  " years old");
+
+
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+//Console.WriteLine("Please enter your first name:");
